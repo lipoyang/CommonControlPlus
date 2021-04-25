191bcc7b9881f0bd3d0b1a3e8258da195b869b3a
lipoyang <lipoyang1453@gmail.com>
2021-04-25 21:04:10 +0900

    add files

---
+++ CommonControlPlus/TextBoxNumeric.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel; // [Browsable(true)]
+
+namespace CommonControlPlus
+{
+    /// <summary>
+    /// 数値用の拡張版テキストボックス (ジェネリッククラス)
+    /// </summary>
+    /// <typeparam name="Type">数値の型 (int, double, decimal のいずれか)</typeparam>
+    public class TextBoxNumeric<Type> : TextBoxPlus where Type : struct, IComparable, IFormattable
+    {
+        #region イベント
+
+        /// <summary>
+        /// 入力値チェック関数の型
+        /// </summary>
+        /// <param name="inputVal">入力された数値</param>
+        /// <returns>OK(true)かNG(false)か</returns>
+        public delegate bool InputValueCheckFunction(Type inputVal);
+
+        /// <summary>
+        /// 入力値チェック関数をここに設定します
+        /// </summary>
+        [Category("拡張機能")]
+        [Browsable(true)]
+        event InputValueCheckFunction InputValueCheck = null;
+
+        #endregion
+
+        #region プロパティ
+
+        /// <summary>
+        /// 入力できる最大値
+        /// </summary>
+        [Category("拡張機能")]
+        [Browsable(true)]
+        public Type? MaxValue { set; get; } = null;
+
+        /// <summary>
+        /// 入力できる最小値
+        /// </summary>
+        [Category("拡張機能")]
+        [Browsable(true)]
+        public Type? MinValue { set; get; } = null;
+
+        /// <summary>
+        /// 入力できる値の最小幅
+        /// </summary>
+        [Category("拡張機能")]
+        [Browsable(true)]
+        public Type? StepValue { set; get; } = null;
+
+        /// <summary>
+        /// 数値
+        /// </summary>
+        [Category("拡張機能")]
+        [Browsable(true)]
+        public Type? Value
+        {
+            get
+            {
+                return _Value;
+            }
+            set
+            {
+                _Value = value;
+                this.Text = value.ToString();
+            }
+        }
+
+        #endregion
+
+        #region 内部処理
+
+        // 数値
+        private Type? _Value = null;
+
+        // 既定の入力値チェック
+        private bool DefaultInputCheck(Type inputVal)
+        {
+            // 最小値・最大値のチェック
+            if ((MinValue != null) && (MaxValue != null))
+            {
+                if ((inputVal.CompareTo((Type)MinValue) < 0) ||
+                   (inputVal.CompareTo((Type)MaxValue) > 0))
+                {
+                    ErrorMessage = MinValue.ToString() + "～" +
+                                   MaxValue.ToString() + "の範囲の値を入力してください";
+                    return false;
+                }
+            }
+            // 最小値のみのチェック
+            else if (MinValue != null)
+            {
+                if (inputVal.CompareTo((Type)MinValue) < 0)
+                {
+                    ErrorMessage = MinValue.ToString() + "以上の値を入力してください";
+                    return false;
+                }
+            }
+            // 最大値のみのチェック
+            else if (MaxValue != null)
+            {
+                if (inputVal.CompareTo((Type)MaxValue) > 0)
+                {
+                    ErrorMessage = MaxValue.ToString() + "以下の値を入力してください";
+                    return false;
+                }
+            }
+            // 最小ステップのチェック
+            if ((StepValue != null) && (StepValue != (dynamic)0) &&
+                (((dynamic)inputVal % StepValue) != 0))
+            {
+                ErrorMessage = StepValue.ToString() + "の倍数を入力してください";
+                return false;
+            }
+            return true;
+        }
+
+        // 入力チェックと値の更新
+        override protected bool InputCheckAndUpdate(string text)
+        {
+            // 数値への変換
+            Type inputVal;
+            try
+            {
+                var converter = TypeDescriptor.GetConverter(typeof(Type));
+                if (converter != null)
+                {
+                    inputVal = (Type)converter.ConvertFromString(text);
+                }
+                else
+                {
+                    ErrorMessage = "予期しないエラーが発生しました";
+                    return false;
+                }
+            }
+            catch
+            {
+                if (typeof(Type) == typeof(int))
+                {
+                    ErrorMessage = "整数値を入力してください";
+                }
+                else
+                {
+                    ErrorMessage = "数値を入力してください";
+                }
+                return false;
+            }
+
+            bool result = true;
+            if (InputValueCheck != null)
+            {
+                // ユーザー定義の入力値チェック
+                result = InputValueCheck(inputVal);
+            }
+            else
+            {
+                // 既定の入力値チェック
+                result = DefaultInputCheck(inputVal);
+            }
+            if (result)
+            {
+                _Value = inputVal; // 値を更新
+            }
+            else
+            {
+                ErrorMessageOutput();
+                this.Text = _Value.ToString(); // 前回のテキストに戻す
+            }
+            return result;
+        }
+
+        #endregion
+    }
+
+
+    /// <summary>
+    /// double型数値用の拡張版テキストボックス
+    /// </summary>
+    public class TextBoxDouble : TextBoxNumeric<double>
+    {
+    }
+
+    /// <summary>
+    /// decimal型数値用の拡張版テキストボックス
+    /// </summary>
+    public class TextBoxDecimal : TextBoxNumeric<decimal>
+    {
+    }
+
+    /// <summary>
+    /// int型数値用の拡張版テキストボックス
+    /// </summary>
+    public class TextBoxInteger : TextBoxNumeric<int>
+    {
+    }
+
+}
---
+++ CommonControlPlus/TextBoxPlus.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel; // [Browsable(true)]
+
+namespace CommonControlPlus
+{
+    /// <summary>
+    /// 拡張版テキストボックス
+    /// </summary>
+    [DefaultEvent("Changed")]
+    public class TextBoxPlus : TextBox
+    {
+        #region イベント
+
+        /// <summary>
+        /// 値の変更が確定したときに発生するイベント
+        /// </summary>
+        [Category("拡張機能")]
+        [Browsable(true)]
+        public event EventHandler Changed = delegate { };
+
+        /// <summary>
+        /// 入力文字列チェック関数の型
+        /// </summary>
+        /// <param name="inputText">入力された文字列</param>
+        /// <returns>OK(true)かNG(false)か</returns>
+        public delegate bool InputTextCheckFunction(string inputText);
+
+        /// <summary>
+        /// 入力文字列チェック関数をここに設定します
+        /// </summary>
+        [Category("拡張機能")]
+        [Browsable(true)]
+        event InputTextCheckFunction InputTextCheck = null;
+
+        #endregion
+
+        #region プロパティ
+
+        /// <summary>
+        /// 入力エラー時にメッセージボックスでメッセージを出すか？
+        /// </summary>
+        [Category("拡張機能")]
+        [Browsable(true)]
+        public bool ErrorMessageBoxEnabled { set; get; } = false;
+
+        /// <summary>
+        /// 入力エラー時にコンソール出力にメッセージを出すか？
+        /// </summary>
+        [Category("拡張機能")]
+        [Browsable(true)]
+        public bool ErrorOutputEnabled { set; get; } = false;
+
+        /// <summary>
+        /// エラーメッセージをここに設定します
+        /// </summary>
+        [Category("拡張機能")]
+        [Browsable(true)]
+        public string ErrorMessage { set; get; } = "";
+        #endregion
+
+        #region コンストラクタ
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public TextBoxPlus()
+        {
+            this.InitializeComponent();
+            this.CausesValidation = true; // 検証イベントを有効
+        }
+        #endregion
+
+        #region 内部処理
+
+        // 前回のテキスト (フォーカスが外れたときの判定用)
+        private string OldText = "";
+
+        // 初期化
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TextBoxPlus
+            // 
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxPlus_KeyDown);
+            this.Validating += new System.ComponentModel.CancelEventHandler(this.TextBoxPlus_Validating);
+            this.ResumeLayout(false);
+
+        }
+
+        // キーが押されたとき
+        private void TextBoxPlus_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Enterキーが押されたか？
+            if (e.KeyCode == Keys.Enter)
+            {
+                // 前回の値から変更されているか？
+                if (OldText != this.Text)
+                {
+                    // 入力チェックと値の更新
+                    if (InputCheckAndUpdate(this.Text))
+                    {
+                        Changed(sender, e); // イベント発行
+                    }
+                }
+            }
+        }
+
+        // フォーカスが外れたときの検証イベント
+        private void TextBoxPlus_Validating(object sender, CancelEventArgs e)
+        {
+            // 前回の値から変更されているか？
+            if (OldText != this.Text)
+            {
+                // 入力チェックと値の更新
+                if (InputCheckAndUpdate(this.Text))
+                {
+                    Changed(sender, e); // イベント発行
+                }
+                else
+                {
+                    e.Cancel = true; // 検証の結果、キャンセルを返す
+                }
+            }
+        }
+
+        // エラーメッセージの出力
+        protected void ErrorMessageOutput()
+        {
+            if (ErrorMessage != "")
+            {
+                if (ErrorMessageBoxEnabled)
+                {
+                    MessageBox.Show(ErrorMessage, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                if (ErrorOutputEnabled)
+                {
+                    Console.WriteLine(this.Name + ": " + ErrorMessage);
+                }
+            }
+        }
+
+        // 入力チェックと値の更新
+        virtual protected bool InputCheckAndUpdate(string text)
+        {
+            bool result = true;
+            if (InputTextCheck != null)
+            {
+                // ユーザー定義の入力チェック
+                result = InputTextCheck(text);
+            }
+            if (result)
+            {
+                OldText = this.Text; // 前回のテキストを更新
+            }
+            else
+            {
+                ErrorMessageOutput();
+                this.Text = OldText; // 前回のテキストに戻す
+            }
+            return result;
+        }
+
+        #endregion
+    }
+}
